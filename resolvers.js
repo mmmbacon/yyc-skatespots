64eb384a65311ed035dd190264dd13e6fa70d554
Brandon Macdonald <bmacdonald1986@gmail.com>
2021-07-14 09:50:26 -0600

    Initial Push - Configured Apollo Server with GraphQL

---
+++ resolvers.js
@@ -0,0 +1,12 @@
+const user = {
+  _id: "1",
+  name: "Reed",
+  email: "reddberger@gmail.com",
+  picture: "https://cloudinary.com/asdf"
+}
+
+module.exports = {
+  Query: {
+    me: ()=> user
+  }
+}
